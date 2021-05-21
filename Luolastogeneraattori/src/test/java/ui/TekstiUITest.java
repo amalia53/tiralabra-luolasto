11641fc77e8accd19027fe94f2b0a1328a84c0a9
Amalia Ahola <amalia.ahola@gmail.com>
2021-05-21 17:56:59 +0300

    gradle scanner testin poisto

--- Luolastogeneraattori/src/test/java/ui/TekstiUITest.java
+++ Luolastogeneraattori/src/test/java/ui/TekstiUITest.java
@@ -12,13 +12,6 @@ public class TekstiUITest {
 	@Before
     public void setUp() {
         ui = new TekstiUI(new Scanner(System.in));
-    }
-	
-	@Test
-	public void kysyyUudelleenJosNegatiivinenSyote() {
-		String syote = "-1";
-	    System.setIn(new ByteArrayInputStream(syote.getBytes()));
-		Assert.assertEquals(ui.kysyKoko(), "Anna luolaston koko: ");
 	}
 
 }
