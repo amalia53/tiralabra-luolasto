047d37de92257bd8d4d7c3bd47a5c6a11c03df18
Amalia Ahola <amalia.ahola@gmail.com>
2021-05-28 18:16:11 +0300

    yhtenäisyyden tarkistus ja pienien erillisten luolien poisto

--- Luolastogeneraattori/src/main/java/generaattori/Generaattori.java
+++ Luolastogeneraattori/src/main/java/generaattori/Generaattori.java
@@ -15,7 +15,7 @@ public class Generaattori {
 	 * Generoidaan luolasto. Alustetaan luolasto ja parannetaan sitä
 	 * kierroksia-luokkamuuttujan määrittämän määrän kertaa.
 	 * 
-	 * @param koko		luolaston koko
+	 * @param koko luolaston koko
 	 *
 	 * @return luolasto-matriisi
 	 */
@@ -26,6 +26,7 @@ public class Generaattori {
 		for (int i = 0; i < kierroksia; i++) {
 			luolasto = paranna(luolasto);
 		}
+		luolasto = teeYhtenaiseksi(luolasto);
 		return luolasto;
 	}
 
@@ -55,7 +56,7 @@ public class Generaattori {
 	 * luokkamuuttujan luolaRaja arvo. Mikäli ruutu on luola, muutetaan se seinäksi,
 	 * jos laskettu seinien määrä on suurempu kuin luokkamuuttujan seinaRaja arvo
 	 * 
-	 * @param luolasto		paranneltava luolasto-matriisi
+	 * @param luolasto paranneltava luolasto-matriisi
 	 *
 	 * @return paranneltu luolasto-matriisi
 	 */
@@ -80,9 +81,11 @@ public class Generaattori {
 	 * kaikki kahdeksan ruutua, jotka ovat ruudun naapureina. Ei käsitellä itse
 	 * ruutua. Mikäli naapuriruutu on seinä, lisätään laskuriin seinä.
 	 * 
-	 * @param luolasto		paranneltava luolasto-matriisi
-	 * @param x				ruudun x-koordinaatti
-	 * @param y 			ruudun y-koordinaatti
+	 * @param luolasto paranneltava luolasto-matriisi
+	 * 
+	 * @param x ruudun x-koordinaatti
+	 * 
+	 * @param y ruudun y-koordinaatti
 	 *
 	 * @return seinien määrä annetun ruudun naapureissa
 	 */
@@ -108,7 +111,11 @@ public class Generaattori {
 	 * Tutkitaan onko syötteenä annettu ruutu seinä. Tulkitaan luolaston
 	 * ulkopuolella olevat ruudut seinäksi.
 	 * 
-	 * @param paranneltava luolasto-matriisi, ruudun x ja y -koordinaatit
+	 * @param luolasto paranneltava luolasto-matriisi
+	 * 
+	 * @param x ruudun x-koordinaatti
+	 * 
+	 * @param y ruudun y-koordinaatti
 	 *
 	 * @return true, jos on seinä; false, jos luola
 	 */
@@ -117,4 +124,128 @@ public class Generaattori {
 		return x < 0 || y < 0 || x >= luolasto.length || y >= luolasto.length || luolasto[x][y];
 	}
 
+	public boolean[][] teeYhtenaiseksi(boolean[][] luolasto) {
+		tulosta(luolasto);
+		int luolanKoko;
+		int luolia = 0;
+		boolean[][] vierailtu = new boolean[luolasto.length][luolasto.length];		
+		while (true) {
+			int x_alku = haeAloitusX(luolasto, vierailtu);
+			if (x_alku == -1) {
+				break;
+			}
+			int y_alku = haeAloitusY(luolasto, vierailtu);
+			boolean[][] vierailtuKierroksella = new boolean[luolasto.length][luolasto.length];
+			luolanKoko = tarkistaYhtenaisyys(x_alku, y_alku, luolasto, vierailtuKierroksella);
+			luolia++;
+			lisaaVierailtu(luolasto, vierailtu, vierailtuKierroksella);
+			System.out.println();
+			System.out.println("Luolan koko: " + luolanKoko);
+			if (luolanKoko < 1.5 * luolasto.length) {
+				System.out.println("Poistetaan");
+				poistaLuola(luolasto, vierailtuKierroksella);
+			} else {
+				if (luolia > 1) {
+					System.out.println("Yhdistetään");
+					yhdistaLuola(luolasto, vierailtuKierroksella);
+				}
+			}
+		}
+		System.out.println("Luolia oli " + luolia);
+		return luolasto;
+	}
+
+	public boolean[][] poistaLuola(boolean[][] luolasto, boolean[][] vierailtuKierroksella) {
+		for (int x = 0; x < luolasto.length; x++) {
+			for (int y = 0; y < luolasto.length; y++) {
+				if (!luolasto[x][y] && vierailtuKierroksella[x][y]) {
+					luolasto[x][y] = true;
+				}
+			}
+		}
+		return luolasto;
+	}
+	
+	public boolean[][] yhdistaLuola(boolean[][] luolasto, boolean[][] vierailtuKierroksella) {
+		return luolasto;
+	}
+	
+	public boolean[][] lisaaVierailtu(boolean[][] luolasto, boolean[][] vierailtu, boolean[][] vierailtuKierroksella) {
+		for (int x = 0; x < luolasto.length; x++) {
+			for (int y = 0; y < luolasto.length; y++) {
+				if (!luolasto[x][y] && vierailtuKierroksella[x][y]) {
+					vierailtu[x][y] = true;
+				}
+			}
+		}
+		return vierailtu;
+	}
+
+	public int haeAloitusX(boolean[][] luolasto, boolean[][] vierailtu) {
+		for (int x = 0; x < luolasto.length; x++) {
+			for (int y = 0; y < luolasto.length; y++) {
+				if (!luolasto[x][y] && !vierailtu[x][y]) {
+					return x;
+				}
+			}
+		}
+		return -1;
+	}
+
+	public int haeAloitusY(boolean[][] luolasto, boolean[][] vierailtu) {
+		for (int x = 0; x < luolasto.length; x++) {
+			for (int y = 0; y < luolasto.length; y++) {
+				if (!luolasto[x][y] && !vierailtu[x][y]) {
+					return y;
+				}
+			}
+		}
+		return -1;
+	}
+
+	public int tarkistaYhtenaisyys(int x, int y, boolean[][] luolasto, boolean[][] vierailtu) {
+		if (onSeina(luolasto, x, y) || vierailtu[x][y]) {
+			return 0;
+		}
+		vierailtu[x][y] = true;
+//		if (!onSeina(luolasto, x, y + 1) && !vierailtu[x][y + 1]) {
+//			tarkistaYhtenaisyys(x, y + 1, luolasto, vierailtu, koko);
+//		}
+//		if (!onSeina(luolasto, x, y - 1) && !vierailtu[x][y - 1]) {
+//			tarkistaYhtenaisyys(x, y - 1, luolasto, vierailtu, koko);
+//		}
+//		if (!onSeina(luolasto, x + 1, y) && !vierailtu[x + 1][y]) {
+//			tarkistaYhtenaisyys(x + 1, y, luolasto, vierailtu, koko);
+//		}
+//		if (!onSeina(luolasto, x - 1, y) && !vierailtu[x - 1][y]) {
+//			tarkistaYhtenaisyys(x - 1, y, luolasto, vierailtu, koko);
+//		}
+		return 1 + tarkistaYhtenaisyys(x, y + 1, luolasto, vierailtu)
+				+ tarkistaYhtenaisyys(x, y - 1, luolasto, vierailtu)
+				+ tarkistaYhtenaisyys(x + 1, y, luolasto, vierailtu)
+				+ tarkistaYhtenaisyys(x - 1, y, luolasto, vierailtu);
+	}
+
+	public void tulosta(boolean[][] luolasto) {
+		System.out.println();
+		for (int i = 0; i < luolasto.length + 2; i++) {
+			System.out.print("-");
+		}
+		for (int x = 0; x < luolasto.length; x++) {
+			System.out.println();
+			System.out.print("|");
+			for (int y = 0; y < luolasto.length; y++) {
+				if (luolasto[x][y]) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.print("|");
+		}
+		System.out.println();
+		for (int i = 0; i < luolasto.length + 2; i++) {
+			System.out.print("-");
+		}
+	}
 }
