581300bdd0adbafe0bb0093f2d2031ccadd083ad
Amalia Ahola <amalia.ahola@gmail.com>
2021-06-27 22:04:48 +0300

    testejä

--- Luolastogeneraattori/src/test/java/generaattorit/CAGeneraattoriTest.java
+++ Luolastogeneraattori/src/test/java/generaattorit/CAGeneraattoriTest.java
@@ -2,8 +2,10 @@ package generaattorit;
 
 import org.junit.*;
 
+import tietorakenteet.Koordinantti;
+
 public class CAGeneraattoriTest {
-	
+
 	CAGeneraattori gen;
 	boolean[][] testiLuolasto;
 	boolean[][] testiLuolastoEiYhtenainen;
@@ -15,58 +17,57 @@ public class CAGeneraattoriTest {
 		luoTestiLuolasto();
 		luoTestiLuolastoEiYhtenainen();
 	}
-	
+
 	@Test
 	public void laskeNaapuritJotkaSeiniaPalauttaaOikein() {
 		int seinat = gen.laskeNaapuritJotkaSeinia(testiLuolasto, 2, 2);
 		Assert.assertEquals(seinat, 3);
 	}
-	
+
 	@Test
 	public void laskeNaapuritJotkaSeiniaPalauttaaOikeinKunYlaNurkka() {
 		int seinat = gen.laskeNaapuritJotkaSeinia(testiLuolasto, 0, 0);
 		Assert.assertEquals(seinat, 6);
 	}
-	
+
 	@Test
 	public void laskeNaapuritJotkaSeiniaPalauttaaOikeinKunAlaNurkka() {
 		int seinat = gen.laskeNaapuritJotkaSeinia(testiLuolasto, 4, 4);
 		Assert.assertEquals(seinat, 5);
 	}
-	
-	
+
 	@Test
 	public void parannaMuuttaaLuolaksiJosNaapureistaAlle4OnSeinia() {
 		Assert.assertEquals(testiLuolasto[3][2], true);
 		testiLuolasto = gen.paranna(testiLuolasto);
 		Assert.assertEquals(testiLuolasto[3][2], false);
 	}
-	
+
 	@Test
 	public void parannaMuuttaaSeinaksiJosNaapureistaYli4OnSeinia() {
 		Assert.assertEquals(testiLuolasto[0][0], false);
 		testiLuolasto = gen.paranna(testiLuolasto);
 		Assert.assertEquals(testiLuolasto[0][0], true);
 	}
-	
+
 	@Test
 	public void parannaEiMuutaLuolaksiJosNaapureistaAlle4OnSeinia() {
 		Assert.assertEquals(testiLuolasto[0][2], true);
 		testiLuolasto = gen.paranna(testiLuolasto);
 		Assert.assertEquals(testiLuolasto[0][2], true);
 	}
-	
+
 	@Test
 	public void parannaEiMuutaSeinaksiJosNaapureistaYli4OnSeinia() {
 		Assert.assertEquals(testiLuolasto[1][1], false);
 		testiLuolasto = gen.paranna(testiLuolasto);
 		Assert.assertEquals(testiLuolasto[1][1], false);
 	}
-	
+
 	@Test
 	public void lisaaVierailtuLisaaUudellaKierroksellaLoytyneenLuolanMatriisiin() {
 		boolean[][] vierailtuKierroksella = new boolean[5][5];
-		boolean[][] vierailtu = new boolean[5][5];		
+		boolean[][] vierailtu = new boolean[5][5];
 		vierailtuKierroksella[2][1] = true;
 		vierailtuKierroksella[3][1] = true;
 		vierailtuKierroksella[4][0] = true;
@@ -75,7 +76,7 @@ public class CAGeneraattoriTest {
 		gen.lisaaVierailtu(testiLuolastoEiYhtenainen, vierailtu, vierailtuKierroksella);
 		Assert.assertEquals(vierailtu[2][1], true);
 	}
-	
+
 	@Test
 	public void poistaLuolaPoistaaLiianPienenErillisenLuolan() {
 		boolean[][] vierailtuKierroksella = new boolean[5][5];
@@ -87,7 +88,7 @@ public class CAGeneraattoriTest {
 		gen.poistaLuola(testiLuolastoEiYhtenainen, vierailtuKierroksella);
 		Assert.assertEquals(testiLuolastoEiYhtenainen[3][1], true);
 	}
-	
+
 	@Test
 	public void yhdistaYhdistaaSuurenErillisenLuolan() {
 		boolean[][] vierailtuKierroksella = new boolean[5][5];
@@ -99,17 +100,17 @@ public class CAGeneraattoriTest {
 		gen.poistaLuola(testiLuolastoEiYhtenainen, vierailtuKierroksella);
 		Assert.assertEquals(testiLuolastoEiYhtenainen[3][1], true);
 	}
-	
+
 	@Test
 	public void etsiYhtenainenLuolaJaKokoPalauttaaYhtenaisenLuolanKoon() {
-		boolean[][] vierailtu = new boolean[5][5];	
+		boolean[][] vierailtu = new boolean[5][5];
 		Assert.assertEquals(gen.etsiYhtenainenLuolaJaKoko(0, 0, testiLuolastoEiYhtenainen, vierailtu), 11);
 	}
-	
+
 	@Test
 	public void haeAloitusAntaaVierailemattomanLuolanXKoordinaatin() {
-		boolean[][] vierailtu = new boolean[5][5];	
-		Assert.assertEquals(gen.haeAloitus(testiLuolastoEiYhtenainen, vierailtu).getX(),0);
+		boolean[][] vierailtu = new boolean[5][5];
+		Assert.assertEquals(gen.haeAloitus(testiLuolastoEiYhtenainen, vierailtu).getX(), 0);
 		vierailtu[0][0] = true;
 		vierailtu[0][1] = true;
 		vierailtu[0][2] = true;
@@ -121,13 +122,13 @@ public class CAGeneraattoriTest {
 		vierailtu[2][4] = true;
 		vierailtu[3][3] = true;
 		vierailtu[3][4] = true;
-		Assert.assertEquals(gen.haeAloitus(testiLuolastoEiYhtenainen, vierailtu).getX(),2);
+		Assert.assertEquals(gen.haeAloitus(testiLuolastoEiYhtenainen, vierailtu).getX(), 2);
 	}
-	
+
 	@Test
 	public void haeAloitusYAntaaVierailemattomanLuolanYKoordinaatin() {
-		boolean[][] vierailtu = new boolean[5][5];	
-		Assert.assertEquals(gen.haeAloitus(testiLuolastoEiYhtenainen, vierailtu).getY(),0);
+		boolean[][] vierailtu = new boolean[5][5];
+		Assert.assertEquals(gen.haeAloitus(testiLuolastoEiYhtenainen, vierailtu).getY(), 0);
 		vierailtu[0][0] = true;
 		vierailtu[0][1] = true;
 		vierailtu[0][2] = true;
@@ -139,9 +140,9 @@ public class CAGeneraattoriTest {
 		vierailtu[2][4] = true;
 		vierailtu[3][3] = true;
 		vierailtu[3][4] = true;
-		Assert.assertEquals(gen.haeAloitus(testiLuolastoEiYhtenainen, vierailtu).getY(),1);
+		Assert.assertEquals(gen.haeAloitus(testiLuolastoEiYhtenainen, vierailtu).getY(), 1);
 	}
-	
+
 	@Test
 	public void teeYhtenaiseksiLoytaaPienenErillisenLuolanJaPoistaaSen() {
 		Assert.assertEquals(testiLuolastoEiYhtenainen[3][1], false);
@@ -149,15 +150,45 @@ public class CAGeneraattoriTest {
 		Assert.assertEquals(testiLuolastoEiYhtenainen[3][1], true);
 	}
 	
-//	@Test
-//	public void teeYhtenaiseksiLoytaaSuurenErillisenLuolanJaYhdistaaSen() {
-//		luoTestiLuolastoKaksiSuurta();
-//		float luolanOsuus = gen.tarkistaLuolanOsuus(testiLuolastoKaksiSuurta);
-//		gen.teeYhtenaiseksi(testiLuolastoKaksiSuurta);
-//		float luolanOsuusNyt = gen.tarkistaLuolanOsuus(testiLuolastoKaksiSuurta);
-//		boolean yhdistetty = luolanOsuusNyt > luolanOsuus;
-//		Assert.assertEquals(yhdistetty, true);
-//	}
+	@Test
+	public void teeYhtenaiseksiYhdistaaLuolatJosSuuretErillisetLuolat() {
+		luoTestiLuolastoKaksiSuurta();
+		Assert.assertEquals(testiLuolastoKaksiSuurta[0][2], true);
+		testiLuolastoKaksiSuurta = gen.teeYhtenaiseksi(testiLuolastoKaksiSuurta);
+		Assert.assertEquals(testiLuolastoKaksiSuurta[0][2], false);
+	}
+
+	@Test
+	public void yhdistaLuoTunnelinErillistenLuolastojenValille() {
+		luoTestiLuolastoKaksiSuurta();
+		boolean[][] vierailtu = new boolean[5][5];
+		vierailtu[0][3] = true;
+		vierailtu[0][4] = true;
+		vierailtu[1][3] = true;
+		vierailtu[1][4] = true;
+		vierailtu[2][3] = true;
+		vierailtu[2][4] = true;
+		vierailtu[3][3] = true;
+		vierailtu[3][4] = true;
+		vierailtu[4][3] = true;
+		vierailtu[4][4] = true;
+		Koordinantti aloitus = gen.haeAloitus(testiLuolastoKaksiSuurta, vierailtu);
+		Assert.assertEquals(testiLuolastoKaksiSuurta[0][2], true);
+		testiLuolastoKaksiSuurta = gen.yhdista(testiLuolastoKaksiSuurta, aloitus, vierailtu);
+		Assert.assertEquals(testiLuolastoKaksiSuurta[0][2], false);
+	}
+	
+	@Test
+	public void tarkistaLuolanOsuusPalauttaaOikeanProsentin() {
+		boolean[][] luolasto = new boolean[2][2];
+		Assert.assertEquals(gen.tarkistaLuolanOsuus(luolasto), 1, 0);
+		luolasto[0][0] = true;
+		Assert.assertEquals(gen.tarkistaLuolanOsuus(luolasto), 0.75, 0);
+		luolasto[1][1] = true;
+		float puolet = 0.5f;
+		Assert.assertEquals(gen.tarkistaLuolanOsuus(luolasto), puolet, 0);
+	}
+	
 
 	public void luoTestiLuolasto() {
 		testiLuolasto = new boolean[5][5];
@@ -174,7 +205,7 @@ public class CAGeneraattoriTest {
 		testiLuolasto[3][2] = true;
 		testiLuolasto[4][0] = true;
 	}
-	
+
 	public void luoTestiLuolastoEiYhtenainen() {
 		testiLuolastoEiYhtenainen = new boolean[5][5];
 		testiLuolastoEiYhtenainen[1][0] = true;
@@ -188,16 +219,13 @@ public class CAGeneraattoriTest {
 		testiLuolastoEiYhtenainen[4][3] = true;
 		testiLuolastoEiYhtenainen[4][4] = true;
 	}
-	
+
 	public void luoTestiLuolastoKaksiSuurta() {
 		testiLuolastoKaksiSuurta = new boolean[5][5];
-		testiLuolastoEiYhtenainen[0][3] = true;
-		testiLuolastoEiYhtenainen[0][4] = true;
-		testiLuolastoEiYhtenainen[1][3] = true;
-		testiLuolastoEiYhtenainen[2][3] = true;
-		testiLuolastoEiYhtenainen[3][0] = true;
-		testiLuolastoEiYhtenainen[3][1] = true;
-		testiLuolastoEiYhtenainen[3][2] = true;
-		testiLuolastoEiYhtenainen[3][3] = true;
+		testiLuolastoKaksiSuurta[0][2] = true;
+		testiLuolastoKaksiSuurta[1][2] = true;
+		testiLuolastoKaksiSuurta[2][2] = true;
+		testiLuolastoKaksiSuurta[3][2] = true;
+		testiLuolastoKaksiSuurta[4][2] = true;
 	}
 }
