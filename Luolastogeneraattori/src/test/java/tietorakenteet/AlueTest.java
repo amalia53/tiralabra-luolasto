68113a782b6e348b70a02382c3c20fce9520066d
Amalia Ahola <amalia.ahola@gmail.com>
2021-06-27 22:13:35 +0300

    testejä

--- Luolastogeneraattori/src/test/java/tietorakenteet/AlueTest.java
+++ Luolastogeneraattori/src/test/java/tietorakenteet/AlueTest.java
@@ -8,46 +8,54 @@ public class AlueTest {
 	
 	@Before
 	public void SetUp() {
-		juuri = new Alue(0,0,10,10);
+		juuri = new Alue(0,0,30,30);
 	}
 	
-//	@Test
-//	public void jaaAlueJakaaAlueenPienemmiksiAlueiksiJosAlueTarpeeksiSuuri() {
-//		Assert.assertEquals(juuri.getVasen(), null);
-//		Assert.assertEquals(juuri.getOikea(), null);
-//		juuri.jaaAlue();
-//		boolean luotu = juuri.getVasen() != null;
-//		Assert.assertEquals(luotu, true);
-//		luotu = juuri.getOikea() != null;
-//		Assert.assertEquals(luotu, true);
-//	}
-//	
-//	@Test
-//	public void jaaAlueJEiJaaAluettaPienemmiksiAlueiksiJosAlueLiianPieni() {
-//		juuri = new Alue(0,0,5,5);
-//		Assert.assertEquals(juuri.getVasen(), null);
-//		Assert.assertEquals(juuri.getOikea(), null);
-//		juuri.jaaAlue();
-//		boolean luotu = juuri.getVasen() != null;
-//		Assert.assertEquals(luotu, false);
-//		luotu = juuri.getOikea() != null;
-//		Assert.assertEquals(luotu, false);
-//	}
-//	
-//	@Test
-//	public void jakaaAlueenVaakatasossaJosEiVoiPystytasossa() {
-//		juuri = new Alue(0,0,5,8);
-//		juuri.jaaAlue();
-//		Assert.assertEquals(juuri.getOikea().getY(), juuri.getY());
-//		Assert.assertNotEquals(juuri.getOikea().getX(), juuri.getX());
-//	}
-//	
-//	@Test
-//	public void jakaaAlueenPystytasossaJosEiVoiVaakatasossa() {
-//		juuri = new Alue(0,0,10,4);
-//		juuri.jaaAlue();
-//		Assert.assertNotEquals(juuri.getOikea().getY(), juuri.getY());
-//		Assert.assertEquals(juuri.getOikea().getX(), juuri.getX());
-//	}
+	@Test
+	public void jaaAlueJakaaAlueenPienemmiksiAlueiksiJosAlueTarpeeksiSuuri() {
+		Assert.assertEquals(juuri.getVasen(), null);
+		Assert.assertEquals(juuri.getOikea(), null);
+		juuri.jaaAlue();
+		boolean luotu = juuri.getVasen() != null;
+		Assert.assertEquals(luotu, true);
+		luotu = juuri.getOikea() != null;
+		Assert.assertEquals(luotu, true);
+	}
+	
+	@Test
+	public void jaaAlueJEiJaaAluettaPienemmiksiAlueiksiJosAlueLiianPieni() {
+		juuri = new Alue(0,0,5,5);
+		Assert.assertEquals(juuri.getVasen(), null);
+		Assert.assertEquals(juuri.getOikea(), null);
+		juuri.jaaAlue();
+		boolean luotu = juuri.getVasen() != null;
+		Assert.assertEquals(luotu, false);
+		luotu = juuri.getOikea() != null;
+		Assert.assertEquals(luotu, false);
+		juuri = new Alue(0,0,19,19);
+		Assert.assertEquals(juuri.getVasen(), null);
+		Assert.assertEquals(juuri.getOikea(), null);
+		juuri.jaaAlue();
+		luotu = juuri.getVasen() != null;
+		Assert.assertEquals(luotu, false);
+		luotu = juuri.getOikea() != null;
+		Assert.assertEquals(luotu, false);
+	}
+	
+	@Test
+	public void jakaaAlueenVaakatasossaJosEiVoiPystytasossa() {
+		juuri = new Alue(0,0,7,24);
+		juuri.jaaAlue();
+		Assert.assertEquals(juuri.getOikea().getY(), juuri.getY());
+		Assert.assertNotEquals(juuri.getOikea().getX(), juuri.getX());
+	}
+	
+	@Test
+	public void jakaaAlueenPystytasossaJosEiVoiVaakatasossa() {
+		juuri = new Alue(0,0,27,11);
+		juuri.jaaAlue();
+		Assert.assertNotEquals(juuri.getOikea().getY(), juuri.getY());
+		Assert.assertEquals(juuri.getOikea().getX(), juuri.getX());
+	}
 
 }
